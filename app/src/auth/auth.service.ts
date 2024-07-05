817aca2ed838a5ddc2072fe136cd8b65fd3f33eb
Artem Yeremenko <a.yeremenko@asbis.com>
2024-07-05 20:08:44 +0300

    add password hash checks

--- app/src/auth/auth.service.ts
+++ app/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { User } from '../users/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,17 +13,12 @@ export class AuthService {
 
   async validateUser(login: string, pass: string): Promise<Partial<User>> {
     const user = await this.usersService.findByLogin(login);
-    //todo change password validation
-    if (!user || user.password !== pass) {
+    if (!user || !(await bcrypt.compare(pass, user.password))) {
       return null;
     }
     //todo add hiding password
 
     return user;
-  }
-
-  signUp() {
-    //todo
   }
 
   async login(user: Partial<User>): Promise<object> {
