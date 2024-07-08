f7617a6c730d46d2ca8e5ddfc0b4ed837e3774ff
Artem Yeremenko <a.yeremenko@asbis.com>
2024-07-08 18:22:35 +0300

    enable CORS

--- app/src/main.ts
+++ app/src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app.port');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(port);
   console.log('Application is running on port ' + port);
 }
